9574f9901b0ebf03980172c50d7373bb9d665c37
gjevardat <greg.jdf@gmail.com>
2024-10-16 11:44:25 +0200

    add static page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-'use client'
-import React, { useState } from 'react';
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import QuizCard from '@/components/QuizCard';
+import QuizzApp from '@/components/QuizApp';
 
 export type VerbType = {
   verb:string,
@@ -10,6 +6,7 @@ export type VerbType = {
   pastParticiple:string,
   frenchTranslation:string
 }
+
 const irregularVerbs : VerbType[] =[
   { verb: 'arise', pastSimple: 'arose', pastParticiple: 'arisen', frenchTranslation: 'survenir' },
   { verb: 'awake', pastSimple: 'awoke', pastParticiple: 'awoken', frenchTranslation: 'se réveiller' },
@@ -123,117 +120,10 @@ const irregularVerbs : VerbType[] =[
 ]
 ;
 
-type QuizResultProps = {
-  score:number,
-  totalQuestions:number,
-  restartQuiz: ()=>void
+
+export default function Page(){
+
+  return (<QuizzApp verbs={irregularVerbs}/>);
 }
-const QuizResult = ({ score, totalQuestions, restartQuiz }:QuizResultProps) => (
-  <Card className="w-full max-w-md mx-auto my-4">
-    <CardHeader className="text-lg font-bold">Quiz Result</CardHeader>
-    <CardContent>
-      <p>Your score: {score} out of {totalQuestions}</p>
-      <p>Percentage: {((score / totalQuestions) * 100).toFixed(2)}%</p>
-    </CardContent>
-    <CardFooter>
-    <Button onClick={restartQuiz}>Retry Quiz</Button>
-    </CardFooter>
-  </Card>
-);
 
-const App = () => {
-  const [selectedVerbs, setSelectedVerbs] = useState<VerbType[]>([]);
-  const [quizQuestions, setQuizQuestions] = useState<VerbType[]>([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [quizComplete, setQuizComplete] = useState(false);
-  const [quizStarted, setQuizStarted] = useState(false);
 
-  const handleVerbSelection = (verb:VerbType) => {
-    setSelectedVerbs(prev => 
-      prev.includes(verb) ? prev.filter(v => v !== verb) : [...prev, verb]
-    );
-  };
-
-  const startQuiz = () => {
-    
-      const shuffled = [...selectedVerbs].sort(() => 0.5 - Math.random());
-      setQuizQuestions(shuffled.slice(0, selectedVerbs.length));
-      setQuizStarted(true);
-    
-  };
-
-  const handleAnswer = (answers:Partial<VerbType>, correctVerb:VerbType) => {
-    setTimeout(() => {
-      
-      //a bit tricky type script typing thing. powerful !
-      const isCorrect = (Object.keys(answers) as Array<keyof VerbType>).every(
-        field => answers[field] === correctVerb[field]
-      );
-     
-      if (isCorrect) setScore(prev => prev + 1);
-      if (currentQuestionIndex < quizQuestions.length - 1) {
-        setCurrentQuestionIndex(prev => prev + 1);
-      } else {
-        setQuizComplete(true);
-      }
-    }, 2000);
-  };
-
-  const restartQuiz = () => {
-    setSelectedVerbs([]);
-    setCurrentQuestionIndex(0);
-    setScore(0);
-    setQuizComplete(false);
-    setQuizStarted(false);
-    setQuizQuestions([]);
-  };
-
-  return (
-    <div className="container mx-auto  max-w-md p-4">
-      
-      
-      {!quizStarted && (
-        <>
-          <div className="pb-4">
-            <h2 className="text-xl font-semibold mb-2">Selected Verbs : {selectedVerbs.length} out of {irregularVerbs.length}</h2>
-            {irregularVerbs.map(verb => (
-              <Button
-                key={verb.verb}
-                onClick={() => handleVerbSelection(verb)}
-                className={`m-0.5 ${selectedVerbs.includes(verb) ? 'bg-blue-500' : 'bg-gray-300'}`}
-                size={"sm"}
-              >
-                {verb.verb}
-              </Button>
-            ))}
-          </div>
-          
-          
-          
-
-          <Button onClick={startQuiz} disabled={selectedVerbs.length < 1}>
-            Start Quiz
-          </Button>
-        </>
-      )}
-
-      {quizStarted && !quizComplete && quizQuestions.length > 0 && (
-        <QuizCard
-          verb={quizQuestions[currentQuestionIndex]}
-          onAnswer={handleAnswer}
-          currentQuestionIndex={currentQuestionIndex + 1}
-          totalQuestions={selectedVerbs.length}
-        />
-      )}
-
-      {quizComplete && (
-        <>
-          <QuizResult score={score} totalQuestions={selectedVerbs.length} restartQuiz={restartQuiz} />
-        </>
-      )}
-    </div>
-  );
-};
-
-export default App;
